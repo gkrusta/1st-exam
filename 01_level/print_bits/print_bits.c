ad92a6eaf28cfe73544f67b187e0fa33e32c42f8
Gabriela <krustagabriela@gmail.com>
2023-05-13 20:54:30 +0200

    print_besado

---
+++ 01_level/print_bits/print_bits.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void	print_bits(unsigned char octet)
+{
+	unsigned char	i;
+
+	i = 128;
+	while (i > 0)
+	{
+		if (octet & i)
+			write (1, "1", 1);
+		else
+			write (1, "0", 1);
+		i = i >> 1;
+	}
+}
+
+/* int	main(void)
+{
+	unsigned char	byte;
+
+	byte = 2;
+	print_bits(byte);
+	return (0);
+} */
