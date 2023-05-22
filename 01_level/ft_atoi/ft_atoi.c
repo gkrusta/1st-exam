5c61380ba1c2060bc1601f582f2c1cf17dbb6f11
Gabriela <krustagabriela@gmail.com>
2023-05-22 16:28:39 +0200

    added new exercises

---
+++ 01_level/ft_atoi/ft_atoi.c
@@ -0,0 +1,32 @@
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	nb;
+
+	i = 0;
+	sign = 1;
+	nb = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= (-1);
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = (str[i] - '0') + nb * 10;
+		i++;
+	}
+	return (sign * nb);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	char	str[] = "     +1234567asdfgh";
+	printf("result %d", ft_atoi(str));
+	return (0);
+}
